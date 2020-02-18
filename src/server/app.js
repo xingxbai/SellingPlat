eda6221a9dc3cd0cbdda393a0a6a18b5090b794c
baixingxing <email@e920702811@qq.com>
2020-02-18 21:29:40 +0800

    测试app

--- src/server/app.js
+++ src/server/app.js
@@ -3,8 +3,27 @@ const data = require('../../public/table.json')
 const app = express()
 
 app.get('/',(req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.send('hello wolrd')
+    let result = [
+        {
+            id: 1,
+            url: 'https://i.epochtimes.com/assets/uploads/2018/08/20180801_Gaspe_lighthouse2-600x400.jpg'
+        },
+        {
+            id:2,
+            url: 'https://i.epochtimes.com/assets/uploads/2018/08/20180801_Gaspe_lighthouse3-450x600.jpg'
+        },
+        {
+            id:3,
+            url:'https://i.epochtimes.com/assets/uploads/2018/08/20180801_Gaspe_lighthouse3-450x600.jpg'
+        },
+        {
+            id:4,
+            url:'https://i.epochtimes.com/assets/uploads/2018/08/20180801_Gaspe_lighthouse2-600x400.jpg'
+        }
+    ]
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.send(result)
 })
 app.get('/table', (req, res) => {
     console.log(data)
