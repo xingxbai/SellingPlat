004406dd211dd81f4520b936e31a52cf9b206797
15891075984 <920702811@qq.com>
2020-04-27 22:20:57 +0800

    test

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -34,7 +34,7 @@ export default {
     data: function() {
         return {
             param: {
-                username: 'admin',
+                username: 'admin22',
                 password: 'admin',
             },
             rules: {
